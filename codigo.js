6e3ee52075c7dae59fc45a2a28cc73e2b7eead7c
BaltazarDomingo <bdomingo@itba.edu.ar>
2022-05-10 20:28:41 -0300

    commit

--- codigo.js
+++ codigo.js
@@ -35,16 +35,37 @@ console.log(profesor)
 profesor.splice(profesor.length/2,1)
 console.log(profesor)
 
-let mitad = profesor.splice(profesor.length/2,1)
+let mitad = profesor.splice(profesor.length/2,1) 
 console.log(mitad)
 
 let equipo = ['esteban', 'dani', 'nicky', 'guille']
 console.log(equipo)
-let frase = equipo.join(' mulea a ')
+let frase = equipo.join(' mulea a ') //introduce la frase entre cada dato del array
 console.log(frase)
 
 let perros = ['Polo', 'Odin']
-let gatos = ['Dona', 'Michi']
-let mascotas = perros.concat(gatos)
+let gatos = ['Dona', 'Michi'] 
+let mascotas = perros.concat(gatos) // Concatena los arrays
 console.log(mascotas)
 
+let letras = ['a','b','c','d','e']
+let nuevo = letras.slice(letras.length/2,letras.length)
+console.log(letras)
+console.log(nuevo)
+
+let invertido = letras.reverse() //inveirte el array
+console.log(invertido)
+
+console.log(letras.includes('a')) //Va a devolver verdadero ya que 'a' se encuentra dentro del array
+console.log(letras.includes('f')) //Va a devolver falso ya que 'f' no se encuentra dentro del array
+
+if (letras.includes('a')){
+    console.log('la letra esta en el array')
+} else{
+    console.log('la letra no esta en el array')
+}
+
+let letras = ['a','b','c']
+console.log(letras.indexOf('a')) //Va a devolver 0 (Indica el indice en donde esta el elemento)
+console.log(letras.indexOf('b')) //Va a devolver 2 (Indica el indice en donde esta el elemento)
+console.log(letras.indexOf('f')) //Va a devolver -1 (Indica que el elemento no se encuentra en el array)
